eae6bc545313b7fb4050150ea6935909e4e138c7
Sadullah zolfqar <sadullah.zolfqar@gmail.com>
2023-01-14 16:31:16 +0300

    product component

--- FirstApp/src/app/app.module.ts
+++ FirstApp/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations:[AppComponent, ProductsComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- FirstApp/src/app/products/products.component.ts
+++ FirstApp/src/app/products/products.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  name: string;
+  price: Number,
+  image: string
+}
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,4 +13,32 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  products:Product[] = [
+    {
+      name: 'Wireless Gaming Headset',
+      price : 41,
+      image: "https://m.media-amazon.com/images/I/71EMuxMps5L._SX522_.jpg"
+    },
+    {
+      name: 'IPhone 14 Pro Max',
+      price : 1100,
+      image: "https://m.media-amazon.com/images/I/61Vsnjewm7L._AC_SX466_.jpg"
+    }
+  ];
+
+  // isDisabled:boolean = false;
+  search:string = '';
+  isShowing: boolean = true;
+
+  toggleBooks() {
+    this.isShowing = !this.isShowing;
+  }
+
+  // showProduct() {
+  //   this.isDisabled = !this.isDisabled;
+  // }
+
+  // handleInput(event: any) {
+  //   this.search = event.target.value;
+  // }
 }
