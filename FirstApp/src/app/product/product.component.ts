664e8467b8c047a42d79ce5b1f6b5260262ae9fe
Sadullah zolfqar <sadullah.zolfqar@gmail.com>
2023-01-15 13:00:35 +0300

    Angular ngInit and NgDestroy lifehook

--- FirstApp/src/app/product/product.component.ts
+++ FirstApp/src/app/product/product.component.ts
@@ -1,3 +1,5 @@
+import { OnInit } from '@angular/core';
+import { OnDestroy } from '@angular/core';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../types/product';
 
@@ -6,7 +8,25 @@ import { Product } from '../types/product';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnDestroy {
+  constructor() {
+    console.log({ constructor: "contructor"});
+  }
+
+  interval: any = null;
+
+  ngOnInit(): void {
+    console.log({ onInit: "onInit"});
+    // this.interval = setInterval(() => {
+    //   console.log("Hello");
+    // }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    // clearInterval(this.interval);
+    console.log({ destroy: "OnDestroy"});  
+  }
+
   @Input() product: Product = {} as Product;
   @Output() productEmitter = new EventEmitter<Product>();
 
