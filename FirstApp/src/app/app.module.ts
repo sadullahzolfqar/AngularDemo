dc60599386d16d1e3330b4daabf5d0e5d8464475
Sadullah zolfqar <sadullah.zolfqar@gmail.com>
2023-01-15 20:48:08 +0300

    routing added

--- FirstApp/src/app/app.module.ts
+++ FirstApp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { ProductsModule } from "./products/products.module";
 import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations:[AppComponent, CartComponent],
-  imports: [BrowserModule, FormsModule, ProductsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ProductsModule],
   // providers: [ProductsService],
   bootstrap: [AppComponent]
 })
